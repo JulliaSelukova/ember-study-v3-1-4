8f92ba545cfc18515760e700284d1b59eeb70ac5
Julia Selukova <jullia.orekhova@yandex.ru>
2021-11-11 14:20:22 +0500

    fix создания спикера. Работа компонента с копией данных.

--- app/components/speaker-form.js
+++ app/components/speaker-form.js
@@ -4,7 +4,26 @@ export default Component.extend({
     actions: {
         submitForm(e) {
             e.preventDefault();
-            this.onsubmit(this.get('speaker'));
+            this.onsubmit({
+                id: this.get('idSpeaker'),
+                firstName: this.get('firstName'),
+                lastName: this.get('lastName'),
+                secondName: this.get('secondName')
+            });
         }
+    },
+
+    didReceiveAttrs() {
+        this._super(...arguments);
+        // this.set('firstName', this.get('speaker.firstName'));
+        // this.set('lastName', this.get('speaker.lastName'));
+        // this.set('secondName', this.get('speaker.secondName'));
+
+        this.setProperties ({
+            idSpeaker: this.get('speaker.id') ? this.get('speaker.id') : undefined,
+            firstName: this.get('speaker.firstName'),
+            lastName: this.get('speaker.lastName'),
+            secondName: this.get('speaker.secondName')
+        })
     }
 });
