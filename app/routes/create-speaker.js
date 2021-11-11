5d3cf59af43949e7c2a549b9c9a45fa8b09836b9
Julia Selukova <jullia.orekhova@yandex.ru>
2021-11-11 15:39:26 +0500

    Более правильный fix создания спикера c использованием хука model().

--- app/routes/create-speaker.js
+++ app/routes/create-speaker.js
@@ -1,12 +1,12 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
 
 export default Route.extend ({
-  /*model() {
-    return {
-      id: '',
+  model() {
+    return EmberObject.create({
       firstName: '',
       lastName: '',
       secondName: '',
-    };
-  }*/
+    })
+  }
 });
